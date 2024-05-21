2133b4b68d011e989ac571fbaaf52b5ca0e21f1b
Jacob Lin <jal041@ucsd.edu>
2024-05-20 18:11:15 -0700

    test 5

--- _common/include/Mover.hpp
+++ _common/include/Mover.hpp
@@ -17,7 +17,7 @@ public:
 
     Mover(NetworkObject* owner);
 
-    void Update(); // test 4
+    void Update(); // test 5
 
     virtual void Serialize(Packet* packet) override {
         packet->write_float(speed);
