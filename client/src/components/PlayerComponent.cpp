38b90b57b7e069b7eeedb2ec6f237f75fc94c99e
Tim Lacaba <timlacaba123@gmail.com>
2024-06-07 09:35:49 -0700

    fix shake

--- client/src/components/PlayerComponent.cpp
+++ client/src/components/PlayerComponent.cpp
@@ -16,7 +16,7 @@ void PlayerComponent::Update(float deltaTime) {
     glm::vec2 moverDirection = owner->GetComponent<Mover>()->input;
 
     int currHp = owner->GetComponent<Health>()->hp;
-    if (currHp != prevHealth) {
+    if (currHp < prevHealth) {
         GameManager::instance().cam->Shake(0.15f, 0.4f, 0.25f);
         AudioManager::instance().PlayHit();
         prevHealth = currHp;
